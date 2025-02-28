d62573ec3ceb371a0f18f736d6849547e09e5f63
tawsif <sleeping4cat@outlook.com>
2025-02-28 16:31:18 +0600

    Update clean-verse.py

--- clean-verse.py
+++ clean-verse.py
@@ -9,15 +9,15 @@ def clean_lyrics(lyrics):
     return lyrics
 
 def clean_verse():
-    input_file = "/mnt/sdc/verse.jsonl"
-    output_file = "/mnt/sdc/verse-clean.jsonl"
+    input_file = "verse-qm.jsonl"
+    output_file = "verse-clean.jsonl"
     count = 0
 
     with open(input_file, "r", encoding="utf-8") as src, open(output_file, "w", encoding="utf-8") as dest:
         for line in src:
             try:
                 data = json.loads(line)
-                if "lyrics" in data:
+                if "song_id" in data and "lyrics" in data:
                     data["lyrics"] = clean_lyrics(data["lyrics"])
                     count += 1
                 dest.write(json.dumps(data) + "\n")
